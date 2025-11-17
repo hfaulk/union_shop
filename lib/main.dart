8f1f6991e243de83511e588c2bf84063b6125558
Harry <hfaulkner2006@gmail.com>
2025-11-17 14:16:57 +0000

    Refactor file structure: move about.dart and product_page.dart to views directory and update imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:union_shop/product_page.dart';
-import 'package:union_shop/about.dart';
+import 'package:union_shop/views/product_page.dart';
+import 'package:union_shop/views/about.dart';
 
 void main() {
   runApp(const UnionShopApp());
