9ad2dbd2a9d01d664669376e1aec72e7dd315e2c
Harry <hfaulkner2006@gmail.com>
2025-11-24 14:56:53 +0000

    Add HomeConfig model for managing home configuration settings

---
+++ lib/models/home_config.dart
@@ -0,0 +1,29 @@
+import 'package:union_shop/models/collection.dart';
+
+class HomeConfig {
+  final String heroTitle;
+  final String heroDescription;
+  final String heroCollectionId;
+  final List<String> featuredCollectionIds;
+
+  HomeConfig({
+    required this.heroTitle,
+    required this.heroDescription,
+    required this.heroCollectionId,
+    required this.featuredCollectionIds,
+  });
+
+  factory HomeConfig.fromJson(Map<String, dynamic> json) {
+    final hero = json['hero'] as Map<String, dynamic>? ?? {};
+    return HomeConfig(
+      heroTitle: hero['title']?.toString() ?? '',
+      heroDescription: hero['description']?.toString() ?? '',
+      heroCollectionId: hero['collection']?.toString() ?? '',
+      featuredCollectionIds: (json['featuredCollections'] as List?)
+              ?.map((e) => e?.toString() ?? '')
+              .where((s) => s.isNotEmpty)
+              .toList() ??
+          [],
+    );
+  }
+}
