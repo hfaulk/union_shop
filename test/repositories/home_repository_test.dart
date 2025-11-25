a4677501d91612be65d75997decc4d78fa8521bc
Harry Faulkner <hfaulkner2006@gmail.com>
2025-11-25 17:52:59 +0000

    test(repos): add tiny placeholder home repository test to avoid empty file loader error

--- test/repositories/home_repository_test.dart
+++ test/repositories/home_repository_test.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-	test('placeholder home repository test (keeps test loader happy)', () {
-		expect(true, isTrue);
-	});
+  test('placeholder home repository test (keeps test loader happy)', () {
+    expect(true, isTrue);
+  });
 }
-
