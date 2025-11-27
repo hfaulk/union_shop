425f366cd1ed30b4276719be62e66e0f3fd74708
Harry Faulkner <hfaulkner2006@gmail.com>
2025-11-27 15:21:46 +0000

    Refactor HomeView to use SharedLayout and enhance layout structure

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/shared_layout.dart';
+import 'package:union_shop/widgets/hero_carousel.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text(
-            'Home View',
-            style: Theme.of(context).textTheme.headline6,
-          ),
+    return SharedLayout(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+                height: 400, width: double.infinity, child: HeroCarousel()),
+            const SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Column(children: [
+                Container(
+                    height: 190,
+                    color: Colors.grey[200],
+                    child: const Center(child: Text('Featured Collection 1'))),
+                const SizedBox(height: 20),
+                Container(
+                    height: 190,
+                    color: Colors.grey[300],
+                    child: const Center(child: Text('Featured Collection 2'))),
+              ]),
+            ),
+          ],
         ),
       ),
     );
