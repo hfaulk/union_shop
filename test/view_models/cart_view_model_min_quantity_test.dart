23cba71885c0a1dd6395378ca67043a7a15fbb85
Harry Faulkner <hfaulkner2006@gmail.com>
2025-11-28 04:11:28 +0000

    Add widget tests for CartViewModel to verify minimum quantity enforcement

---
+++ test/view_models/cart_view_model_min_quantity_test.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/view_models/cart_view_model.dart';
+import 'package:union_shop/repositories/cart_repository.dart';
+import 'package:union_shop/models/cart_item.dart';
+
+void main() {
+  test('updateQuantity enforces minimum of 1', () async {
+    final tmp = await Directory.systemTemp.createTemp('vm_min_');
+    final repo = CartRepository(documentsDirProvider: () async => tmp);
+    final vm = CartViewModel(repo);
+    await vm.loadCart();
+    final item = CartItem(
+      productId: 'p-min',
+      name: 'Min',
+      price: 1.0,
+      image: '',
+      options: {},
+      quantity: 2,
+      id: 'min1',
+    );
+    await vm.addItem(item);
+    await vm.updateQuantity('min1', 0);
+    expect(vm.items.first.quantity, 1);
+    await tmp.delete(recursive: true);
+  });
+}
