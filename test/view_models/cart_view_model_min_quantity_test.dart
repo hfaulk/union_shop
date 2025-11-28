115ead490f4c2f5dd590d45de30156fea888b53d
Harry Faulkner <hfaulkner2006@gmail.com>
2025-11-28 04:27:08 +0000

    Add initialization for SharedPreferences in cart_view_model_min_quantity_test

--- test/view_models/cart_view_model_min_quantity_test.dart
+++ test/view_models/cart_view_model_min_quantity_test.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:union_shop/view_models/cart_view_model.dart';
 import 'package:union_shop/repositories/cart_repository.dart';
 import 'package:union_shop/models/cart_item.dart';
 
 void main() {
   test('updateQuantity enforces minimum of 1', () async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
     final tmp = await Directory.systemTemp.createTemp('vm_min_');
     final repo = CartRepository(documentsDirProvider: () async => tmp);
     final vm = CartViewModel(repo);
