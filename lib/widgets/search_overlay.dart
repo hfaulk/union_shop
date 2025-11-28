3398d9b7b2a051e1065dd17f7025da961e04292a
Harry Faulkner <hfaulkner2006@gmail.com>
2025-11-28 00:51:25 +0000

    Implement search functionality in SearchOverlay widget

--- lib/widgets/search_overlay.dart
+++ lib/widgets/search_overlay.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/helpers/search_helper.dart';
+import 'package:union_shop/models/product.dart';
 
 class SearchOverlay extends StatefulWidget {
   final VoidCallback? onClose;
@@ -10,6 +12,8 @@ class SearchOverlay extends StatefulWidget {
 
 class _SearchOverlayState extends State<SearchOverlay> {
   String query = '';
+  final SearchHelper _helper = SearchHelper();
+  List<Product> _results = [];
 
   @override
   Widget build(BuildContext context) {
@@ -19,18 +23,41 @@ class _SearchOverlayState extends State<SearchOverlay> {
         child: Container(
           color: Colors.white,
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: Row(
-            children: [
+          child: Column(children: [
+            Row(children: [
               Expanded(
                 child: TextField(
                   decoration: const InputDecoration(hintText: 'Search'),
-                  onChanged: (v) => setState(() => query = v),
+                  onChanged: (v) async {
+                    query = v;
+                    final r = await _helper.search(v);
+                    setState(() => _results = r);
+                  },
                 ),
               ),
               IconButton(
                   icon: const Icon(Icons.close), onPressed: widget.onClose),
-            ],
-          ),
+            ]),
+            if (_results.isNotEmpty)
+              Expanded(
+                child: ListView.separated(
+                  itemCount: _results.length,
+                  separatorBuilder: (_, __) => const Divider(height: 1),
+                  itemBuilder: (c, i) {
+                    final p = _results[i];
+                    return ListTile(
+                      leading: p.imageUrl.isNotEmpty
+                          ? Image.asset(p.imageUrl,
+                              width: 40, height: 40, fit: BoxFit.cover)
+                          : null,
+                      title: Text(p.title),
+                      trailing: Text('£${(p.price / 100).toStringAsFixed(2)}'),
+                      onTap: () {},
+                    );
+                  },
+                ),
+              ),
+          ]),
         ),
       ),
     );
